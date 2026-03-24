1ba2d74f0c1aff9340b6283c3e267c416976fc06
JoHn Pablo Javier Quinonez Paredes <pablo.6098gl@gmail.com>
2026-03-24 09:19:43 -0600

    lo que hace en console log

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-// Variables para la página de Farmacon
+
 let nombreFarmacia = "Farmacon"; // String 1
+
 let slogan = "Tu salud primero"; // String 2
+
 let numeroProductos = 50; // Number
+
 let esAbierta = true; // Boolean
+
 let descuento = null; // Null
+
 let precioEspecial = undefined; // Undefined
 
-// Mostrar valores en consola
+// Mostrar en la  consola
 console.log("Nombre de la farmacia:", nombreFarmacia);
 console.log("Slogan:", slogan);
 console.log("Número de productos:", numeroProductos);
@@ -14,7 +19,7 @@ console.log("¿Está abierta?", esAbierta);
 console.log("Descuento actual:", descuento);
 console.log("Precio especial:", precioEspecial);
 
-// Agregar alerta a botones "Ver más" y "Comprar"
+// Agregar alerta a botones que no hacen nada
 document.querySelectorAll('.btn').forEach(btn => {
     btn.addEventListener('click', (e) => {
         e.preventDefault();
